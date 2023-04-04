477c4ec47ef19a32b957b53ff923cbf25383e1d6
jasper <49565505+wluxie@users.noreply.github.com>
2023-04-04 15:50:44 -0700

    init /dev

---
+++ local/vacuum_pump.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+
+class Mosfet:
+    PUMP = 11
+    VACUUM = 13
+
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(Mosfet.PUMP, GPIO.OUT)
+        GPIO.setup(Mosfet.VACUUM, GPIO.OUT)
+
+    def pump(self, t:float):
+        GPIO.output(Mosfet.PUMP, True)
+        time.sleep(t)
+        GPIO.output(Mosfet.PUMP, False)
+
+    def vacuum(self, t:float):
+        GPIO.output(Mosfet.VACUUM, True)
+        time.sleep(t)
+        GPIO.output(Mosfet.VACUUM, False)
+
+    def __del__(self):
+        GPIO.cleanup()
+
---
+++ main.py
@@ -0,0 +1,3 @@
+from local.camera import Camera
+from local.stepper_motor import StepperMotor
+from local.vacuum_pump import Mosfet
